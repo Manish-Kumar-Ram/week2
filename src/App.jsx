48c88242161aa7502fbaab310a9df96afc1aab68
Manish Kumar Ram <manishkaran9809@gmail.com>
2024-06-06 23:42:15 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Aside from './Components/Aside'
+import Right from './Components/Right'
+function App() {
+
+
+  return (
+   <>
+<Aside></Aside>
+<Right></Right>
+   </>
+  )
+}
+
+export default App
---
+++ src/Components/Aside.jsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faLinkedin, faTwitter, faInstagram, faGithub } from '@fortawesome/free-brands-svg-icons';
+
+function Aside() {
+  return (
+    <>
+      <aside className='container-profile'>
+        <div className="img">
+          <img src='./src//manishimg.jpg' className='im' alt="Manish Kumar Ram" />
+        </div>
+        <div className="profile-info">
+          <h2>
+            Manish Kumar Ram
+          </h2>
+          <input type='text' value="hello" readOnly />
+          <hr />
+          <ul>
+            <li className="icons-info">
+              <div className="icon">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </div>
+              <div className="info">
+                <p>AGE</p>
+                <time>29 years old</time>
+              </div>
+            </li>
+            <li className="icons-info">
+              <div className="icon">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </div>
+              <div className="info">
+                <p>LOCATION</p>
+                <address>Darbhanga, India</address>
+              </div>
+            </li>
+            <li className="icons-info">
+              <div className="icon">
+                <FontAwesomeIcon icon={faLinkedin} />
+              </div>
+              <div className="info">
+                <p>PERSONALITY TYPE</p>
+                <type>INTJ-T</type>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+        <div className="social-icons">
+          <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faLinkedin} />
+          </a>
+          <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faTwitter} />
+          </a>
+          <a href="https://www.instagram.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faInstagram} />
+          </a>
+          <a href="https://www.github.com" target="_blank" rel="noopener noreferrer">
+            <FontAwesomeIcon icon={faGithub} />
+          </a>
+        </div>
+      </aside>
+    </>
+  );
+}
+
+export default Aside;
